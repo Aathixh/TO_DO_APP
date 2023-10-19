b77634ccc4d6c5fcbb3bd2530446c2cfdde2cd7a
Aathish R Viswam <aathishrv@gmail.com>
2023-10-19 11:26:29 +0530

    edit page

--- To_DO/resources/views/tasks/edit.blade.php
+++ To_DO/resources/views/tasks/edit.blade.php
@@ -4,29 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href = "{{ asset('css/edit.css') }}" rel="stylesheet" >
 </head>
-<body>
-    <h1>Edit the task:</h1>
-    <div>
-        @if($errors->any())
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{$errors}}</li>
-                @endforeach
-            </ul>
-        @endif
-    </div>
-    <form action="{{route('tasks.update',['task'=>$task])}}" method="post">
+<body class="background-image">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <h1>Edit the task</h1>
+
+    <div class="d-flex align-items-center justify-content-center">
+    <form action="{{route('tasks.update',['task'=>$task])}}" method="post" autocomplete="off" class="needs-validation">
         @csrf
         @method('put')
-        <div>
-            <label>Task:</label>    
-            <input type="text" name="task" placeholder="Enter the Task" value="{{$task -> tasks}}">
-        </div>
-        <div>
+        
+            <label for="validationCustom03" class="form-label"></label>    
+            <input class="form-control custom-input @error('task') is-invalid @enderror" type="text" name="task" placeholder="Enter the Task" value="{{$task -> tasks}}" id="validationCustom03">
+
+            @error('task')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+        
+        </br>
+        <div class="btn-inp">
             <input type="submit" value="Update" >
         </div>
     </form>
+    </div>
 </body>
+
+<script>
+    (() => {
+        'use strict';
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation');
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    // This line is added to display the Bootstrap validation error messages
+                    form.classList.add('was-validated');
+                }
+            }, false);
+        });
+
+        const taskInput = document.querySelector("#validationCustom03");
+        
+        taskInput.addEventListener("input", function () {
+            // Clear the error message and add success styles when input changes
+            taskInput.classList.remove("is-invalid");
+            taskInput.classList.add("is-valid");
+            const errorFeedback = document.querySelector(".invalid-feedback");
+            if (errorFeedback) {
+                errorFeedback.style.display = "none";
+            }
+        });
+    })();
+</script>
 </html>
