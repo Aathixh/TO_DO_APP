d5e768fa87fdd7d54c86034dc4787f1c64c8d3e8
Aathish R Viswam <aathishrv@gmail.com>
2023-12-09 18:25:17 +0530

    Update web.php

--- To_DO/routes/web.php
+++ To_DO/routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskControllers;
+
+// use App\Http\Controllers\TaskControllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,9 +19,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tasks',[TaskController::class,'index'])->name('tasks.index');
-Route::get('/tasks/create',[TaskController::class,'create'])->name('tasks.create');
-Route::post('/tasks',[TaskController::class,'list'])->name('tasks.list');
-Route::get('/tasks/{task}/edit',[TaskController::class,'edit'])->name('tasks.edit');
-Route::put('/tasks/{task}/update',[TaskController::class,'update'])->name('tasks.update');
-Route::delete('/tasks/{task}/destroy',[TaskController::class,'destroy'])->name('tasks.destroy');
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks', [TaskController::class, 'list'])->name('tasks.list');
+Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::put('/tasks/{task}/update', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/tasks/{task}/destroy', [TaskController::class, 'destroy'])->name('tasks.destroy');
